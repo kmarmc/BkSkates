87f208d17225a7f5978833898251a64be818068d
BkSkate <kauemroberto@gmail.com>
2024-10-31 08:51:15 -0300

    nomeupeito

---
+++ BkSkate/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace BkSkate;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ BkSkate/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BkSkate;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
+
